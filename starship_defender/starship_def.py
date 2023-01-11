e8fc48c4334e46e9d1809270e308a76c59282f57
Iaroslav <GunGalla@gmail.com>
2023-01-11 15:20:49 +0200

    Create invasion fleet

--- starship_defender/starship_def.py
+++ starship_defender/starship_def.py
@@ -86,20 +86,28 @@ class StarshipDefender:
         """Create invasion fleet."""
         # Alien fleet creation
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = int(available_space_x // (1.5 * alien_width))
 
-        # First row of aliens creation
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+        # Define number of rows on the screen
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                             (3 * alien_height) - ship_height)
+        number_rows = int(available_space_y // (1.2 * alien_height))
 
-    def _create_alien(self, alien_number):
+        # Alien fleet creation
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
         """Create separate alien and define its position."""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 1.5 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 1.2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     def _update_screen(self):
